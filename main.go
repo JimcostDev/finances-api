f65a3036e2317b86d8752426d8f566ae0b9a892e
JimcostDev <rjak727@gmail.com>
2025-03-17 13:01:31 +0100

    cors

--- main.go
+++ main.go
@@ -8,10 +8,17 @@ import (
 	"github.com/JimcostDev/finances-api/config"
 	"github.com/JimcostDev/finances-api/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Configura CORS para permitir solicitudes desde localhost:4321
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:4321, https://finances-ui.vercel.app",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	// Conectar a MongoDB
 	config.ConnectDB()
